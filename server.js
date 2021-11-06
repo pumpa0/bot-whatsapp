d6a702495cd04c01b41d9ee8db857768053726d3
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-07 00:02:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ function connect(conn, PORT) {
     app.use(async (req, res) => {
         if (req.path == '/session' && conn.state == 'open') return res.send(conn.base64EncodedAuthInfo())
         if (conn.state == 'open') return res.status(403).send({status: 403, message: 'Bot Telah Tersambung ke whatsapp web anda!' })
-        qrr = await qrcode.toBuffer(_qr)
-        let { url } = (await uploadFile(qrr, { scale: 50 })).result
+        qrr = await qrcode.toBuffer(_qr, { scale: 75 })
+        let { url } = (await uploadFile(qrr)).result
         html = fs.readFileSync('./views/scan.html', 'utf-8')
         res.send(html.replace(/\$QRURL/g, url))
     })
@@ -21,25 +21,6 @@ function connect(conn, PORT) {
     })
     
     let server = app.listen(PORT, () => console.log('App listened on port', PORT))
-    // let io = SocketIO(server)
-    // io.on('connection', socket => {
-    //     let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
-    //     socket.on('disconnect', unpipeEmit)
-    // })
 }
-
-function pipeEmit(event, event2, prefix = '') {
-    let old = event.emit
-    event.emit = function (event, ...args) {
-        old.emit(event, ...args)
-        event2.emit(prefix + event, ...args)
-    }
-    return {
-        unpipeEmit() {
-            event.emit = old
-        }
-    }
-}
-
 
 module.exports = connect

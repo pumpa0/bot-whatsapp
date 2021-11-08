311552b4e77b558718367966ca9da98fe0871f0d
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-08 08:54:52 +0700

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,8 @@
+sudo apt-get install curl -y
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install ffmpeg -y
+sudo apt-get install webp -y
+sudo apt-get install imagemagick -y
+sudo apt-get install nodejs -y
+sudo apt-get install tesseract-ocr-all -y
+npm install

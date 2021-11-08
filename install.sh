8586cdcc229fb2eaf6e7e1c0b15ece1ddc227c7a
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-08 08:47:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-apt-get install ffmpeg -y
-apt-get install webp -y
-apt-get install imagemagick -y
-apt-get install nodejs -y
-apt-get install tesseract-ocr-all -y
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install ffmpeg -y
+sudo apt-get install webp -y
+sudo apt-get install imagemagick -y
+sudo apt-get install nodejs -y
+sudo apt-get install tesseract-ocr-all -y
 npm install

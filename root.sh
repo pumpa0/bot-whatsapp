b798bbcd0930f95b4b1201f0f4ecf68bf750f1ad
I`am Caliph <92326690+Caliph91@users.noreply.github.com>
2021-11-10 11:54:02 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -4,5 +4,5 @@ sudo apt-get install ffmpeg -y
 sudo apt-get install webp -y
 sudo apt-get install imagemagick -y
 sudo apt-get install nodejs -y
-sudo apt-get install tesseract-ocr-all -y
+sudo apt-get install tesseract-ocr -y
 npm install
